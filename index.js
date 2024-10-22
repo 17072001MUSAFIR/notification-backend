cd852de885ddba4922e47ffbcc6d1b49e0f84b4d
Musafir Yoosaf <musafir.yoosaf@gmail.com>
2024-10-22 15:20:58 +0530

    Removed ServiceAccoutKey.json

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const admin = require('firebase-admin');
 const app = express();
 
-// Initialize Firebase Admin with your service account key
-const serviceAccount = require('../serviceAccountKey.json');
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 app.use(express.json());
